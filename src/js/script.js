63b63150829cefa86dcb71ec282344e2faf3891d
Lucas <lucas.lebars@edu.univ-fcomte.fr>
2024-11-20 14:57:23 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -4,15 +4,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const prevButton = document.querySelector('.carousel__button--prev');
     const nextButton = document.querySelector('.carousel__button--next');
     const carouselContainer = document.querySelector('.carousel__container');
-  
-    prevButton.addEventListener('click', function() {
+    const premierItem = document.querySelector(".carousel__item");
+    
+
+    prevButton.addEventListener('click', function() { 
+      const scrollAmount = premierItem.clientWidth;
       carouselContainer.scrollBy({
         left: -200, // Ajustez cette valeur en fonction de la largeur de vos éléments
         behavior: 'smooth'
       });
     });
   
+   
+
+
     nextButton.addEventListener('click', function() {
+      const scrollAmount = premierItem.clientWidth;
       carouselContainer.scrollBy({
         left: 200, // Ajustez cette valeur en fonction de la largeur de vos éléments
         behavior: 'smooth'
